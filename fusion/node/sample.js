e0e9d73e39c8b0dcf511677ecf81f4110369db22
Myles Banner <milob92@gmail.com>
2023-12-11 10:46:28 -0600

    Setting up the npm code

--- fusion/node/sample.js
+++ fusion/node/sample.js
@@ -4,11 +4,11 @@ const yelp = require('yelp-fusion');
 
 // Place holder for Yelp Fusion's API Key. Grab them
 // from https://www.yelp.com/developers/v3/manage_app
-const apiKey = '<YOUR_API_KEY>';
+const apiKey = 'TnGEpgsxdiOtzCsveEyswxornDpPt0mVYi5AC2IXvIADFBAS7v39qjYOy5KQUroSe6u_mfEXhjN3XfhISeR_z0uoDFn9IrzBEBRoypeqZbGXrGw5ssDPs8ZXcFhmZXYx';
 
 const searchRequest = {
-  term:'Four Barrel Coffee',
-  location: 'san francisco, ca'
+  term:'McDonald\'s',
+  location: 'Austin, TX'
 };
 
 const client = yelp.client(apiKey);
